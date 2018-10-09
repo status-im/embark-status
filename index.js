2c7e8a3f34297442ccdbb47eacf09f784327c201
Iuri Matias <iuri.matias@gmail.com>
2018-10-09 17:47:41 -0400

    initial plugin implementation for the new status api

--- index.js
+++ index.js
@@ -8,24 +8,37 @@ module.exports = function(embark) {
 
   embark.registerServiceCheck('Status', function (cb) {
     statusDev.ping((err, state) => {
-      cb({name: `Status.im (${deviceIp})`, status: !!state});
+      let stateName = (!!state ? 'on' : 'off')
+      cb({name: `Status.im (${deviceIp})`, status: stateName});
     });
   });
 
+  embark.events.on('check:backOnline:Status', function () {
+    embark.logger.info("------------------")
+    embark.logger.info("Connected to Status.im mobile app!")
+    embark.logger.info("------------------")
+  });
+
+  embark.events.on('check:wentOffline:Status', function () {
+    embark.logger.error("------------------")
+    embark.logger.error("couldn't connect or lost connection to Status.im mobile app...")
+    embark.logger.error("------------------")
+  });
+
   let dappUrl = `http://${embark.config.webServerConfig.host}:${embark.config.webServerConfig.port}/`;
-  let nodeUrl = `http://localhost:8545/`;
+  let nodeUrl = `http://${embark.config.webServerConfig.host}:8555`;
   let chainName = 'embark';
   let networkId = 1337;
 
-	statusDev.addNetwork('embark', nodeUrl, chainName, networkId, (err, result) => {
-    let statusNetworkId = result['network-id']
-		statusDev.connect(statusNetworkId, (err, result) => {
-			isConnected = true;
-		  statusDev.addDapp(dappUrl, (err, result) => {
-		  });
-		});
-	});
-
-  //embark.events.on("outputDone", function() {
-  //});
+  embark.events.on("outputDone", function() {
+    statusDev.addNetwork('embark', nodeUrl, chainName, networkId, (err, result) => {
+      let statusNetworkId = result['network-id']
+      statusDev.connect(statusNetworkId, (err, result) => {
+        isConnected = true;
+        statusDev.addDapp(dappUrl, (err, result) => {
+          embark.logger.info('you can now access your dapp on the status mobile app')
+        });
+      });
+    });
+  });
 };
--- status.js
+++ status.js
@@ -4,9 +4,11 @@ var StatusDev = function(options) {
 	this.url = `http://${options.ip}:5561`;
 }
 
-StatusDev.prototype.request = function(url, body, method, cb) {
+StatusDev.prototype.request = function(endpoint, body, method, cb) {
+  console.dir("===> " + this.url + endpoint)
+  console.dir(body)
 	request({
-		url: url,
+		url: this.url + endpoint,
 		method: method,
 		timeout: 3000,
 		json: true,
@@ -20,34 +22,34 @@ StatusDev.prototype.request = function(url, body, method, cb) {
 // POST localhost:5561/ping
 // response: {"message": "Pong!"}
 StatusDev.prototype.ping = function(cb) {
-	request('/ping', {}, "POST", cb);
+	this.request('/ping', {}, "POST", cb);
 }
 
 // POST localhost:5561/dapp/open url=<dapp_url>
 // response: { "message": "URL has been opened." }
 StatusDev.prototype.addDapp = function(url, cb) {
-	request('/dapp/open', {url: url}, "POST", cb);
+	this.request('/dapp/open', {url: url}, "POST", cb);
 }
 
 // POST localhost:5561/network name=AnyNetworkName url=http://localhost:3000 chain=mainnet network-id=2
 // response: { "message": "Network has been added.", "network-id": "1535660846036b3c3241022ec5046af53cdb769dcc216" }
 // response: { "message": "Please, check the validity of network information." }
 StatusDev.prototype.addNetwork = function(name, nodeUrl, chainName, networkId, cb) {
-	request('/network', {name: name, url: nodeUrl, chain: chainName, "network-id": networkId}, "POST", cb);
+	this.request('/network', {name: name, url: nodeUrl, chain: chainName, "network-id": networkId}, "POST", cb);
 }
 
 // POST localhost:5561/network/connect id=1535660846036b3c3241022ec5046af53cdb769dcc216
 // response: { "message": "Network has been connected.", "network-id": "1535660846036b3c3241022ec5046af53cdb769dcc216" }
 // response: { "message": "The network id you provided doesn't exist." }
 StatusDev.prototype.connect = function(id, cb) {
-	request('/network/connect', {id: id}, "POST", cb);
+	this.request('/network/connect', {id: id}, "POST", cb);
 }
 
 // DELETE localhost:5561/network id=1535660846036b3c3241022ec5046af53cdb769dcc216
 // response: { "message": "Network has been deleted.", "network-id": "1535660846036b3c3241022ec5046af53cdb769dcc216" }
 // response: { "message": "Cannot delete the provided network." }
 StatusDev.prototype.removeNetwork = function(id, cb) {
-	request('/network', {id: id}, "DELETE", cb);
+	this.request('/network', {id: id}, "DELETE", cb);
 }
 
 module.exports = StatusDev
